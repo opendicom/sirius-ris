34917daa9998d72d96d7426e6260de3e9e3cc0db
Camilo Pifano <camilopifano@gmail.com>
2023-09-26 10:29:51 -0300

    improve access control middleware with penalty time [Backend]

--- backend/src/auth/middlewares.js
+++ backend/src/auth/middlewares.js
@@ -31,8 +31,9 @@ const accessControl = (req, res, next) => {
         //Create final object:
         for (let key in secondRegisteredIPs){
             registeredIPs[secondRegisteredIPs[key][0].toString()] = {
-                counter: secondRegisteredIPs[key][1],
-                last_req: secondRegisteredIPs[key][2]
+                counter     : secondRegisteredIPs[key][1],
+                last_req    : secondRegisteredIPs[key][2],
+                penalty_req : mainServices.stringToBoolean(secondRegisteredIPs[key][3].toString())
             };
         }
     }
@@ -47,31 +48,30 @@ const accessControl = (req, res, next) => {
         let end = Date.now();
         let time_elapsed = Math.round((end - start)/1000);
 
-        //If the time elapsed is <= AUTH_PENALTY_TIME_WINDOW:
-        if(time_elapsed <= mainSettings.AUTH_PENALTY_TIME_WINDOW){
-            //Check if the allowed amount of AUTH_PENALTY_RATE_LIMIT was not exceeded:
-            if(registeredIPs[clientIP].counter >= mainSettings.AUTH_PENALTY_RATE_LIMIT){
-                //Update last request:
-                process.env.REGISTERED_IPS = process.env.REGISTERED_IPS.replace(
-                    clientIP + '|' + registeredIPs[clientIP].counter + '|' + registeredIPs[clientIP].last_req, //String reemplazar
-                    clientIP + '|' + registeredIPs[clientIP].counter + '|' + Date.now(), //String actualizado
-                );
+        //Check if the IP is penalized:
+        if(registeredIPs[clientIP].penalty_req && time_elapsed <= mainSettings.AC_PENALTY_TIME){
+            //Sanction current IP:
+            penaltyClient(req, res, registeredIPs, clientIP);
+            return;
 
-                //Send WARN Message:
-                mainServices.sendConsoleMessage('WARN', currentLang.http.sancioned, registeredIPs);
+        //If the time elapsed is <= AC_TIME_WINDOW:
+        } else if(time_elapsed <= mainSettings.AC_TIME_WINDOW){
+            //Check if the allowed amount of AC_NUMBER_OF_ATTEMPTS was not exceeded:
+            if(registeredIPs[clientIP].counter >= mainSettings.AC_NUMBER_OF_ATTEMPTS){
 
-                //Respond with a sanction message and not allow to continue until the penalty time has expired:
-                res.status(401).send({ success: false, message: currentLang.http.sancioned_msj });
+                //Sanction current IP:
+                penaltyClient(req, res, registeredIPs, clientIP);
                 return;
+
             } else {
-                //Increment counter and update las request:
+                //Increment counter, update last request and preserve penalty_req (don't touch):
                 process.env.REGISTERED_IPS = process.env.REGISTERED_IPS.replace(
                     clientIP + '|' + registeredIPs[clientIP].counter + '|' + registeredIPs[clientIP].last_req, //String reemplazar
                     clientIP + '|' + counter + '|' + Date.now(), //String actualizado
                 );
             }
         } else {
-            //Reset counter and update last request:
+            //Reset counter, update last request and preserve penalty_req (don't touch):
             process.env.REGISTERED_IPS = process.env.REGISTERED_IPS.replace(
                 clientIP + '|' + registeredIPs[clientIP].counter + '|' + registeredIPs[clientIP].last_req, //String reemplazar
                 clientIP + '|' + 1 + '|' + Date.now(), //String actualizado
@@ -79,11 +79,30 @@ const accessControl = (req, res, next) => {
         }
     } else {
         //First time - Initialize counter (Add request to enviroment var):
-        process.env.REGISTERED_IPS = process.env.REGISTERED_IPS.concat(clientIP + '|' + 1 + '|' + Date.now() + ',');
+        //Initialize penalty_req in false
+        process.env.REGISTERED_IPS = process.env.REGISTERED_IPS.concat(clientIP + '|' + 1 + '|' + Date.now() + '|false' + ',');
     }
-    
+
     //If everything is correct, allow to continue:
     next();
+}
+//--------------------------------------------------------------------------------------------------------------------//
+
+//--------------------------------------------------------------------------------------------------------------------//
+// PENALTY CLIENT:
+//--------------------------------------------------------------------------------------------------------------------//
+function penaltyClient(req, res, registeredIPs, clientIP){
+    //Update last request:
+    process.env.REGISTERED_IPS = process.env.REGISTERED_IPS.replace(
+        clientIP + '|' + registeredIPs[clientIP].counter + '|' + registeredIPs[clientIP].last_req + '|' + registeredIPs[clientIP].penalty_req, //String reemplazar
+        clientIP + '|' + registeredIPs[clientIP].counter + '|' + Date.now() + '|true', //String actualizado
+    );
+
+    //Send WARN Message:
+    mainServices.sendConsoleMessage('WARN', currentLang.http.sancioned, registeredIPs);
+
+    //Respond with a sanction message and not allow to continue until the penalty time has expired:
+    res.status(401).send({ success: false, message: currentLang.http.sancioned_msj });
 }
 //--------------------------------------------------------------------------------------------------------------------//
 
