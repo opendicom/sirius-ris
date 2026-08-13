f61bcf1fd58f02e0f65b363b57bc0be25f9c30f3
Mia <miaworkit@gmail.com>
2026-08-13 11:10:52 -0300

    feat(permissions): enable permissions for avg-delay-reports
    - 'avg-delay-reports' is added to the Superuser and Admin stats.
    - Grant 27 (Statistics on report delivery delays) is created to give granular access to other roles.

--- backend/src/main.permissions.js
+++ backend/src/main.permissions.js
@@ -30,7 +30,7 @@ const rolePermissions = {
         mail                    : ['send'],
         exporter                : ['reports'],
         wezen                   : ['studyToken'],
-        stats                   : ['appointment_requests', 'appointments', 'performing', 'reports', 'organizations', 'avg-delay-appointment']
+        stats                   : ['appointment_requests', 'appointments', 'performing', 'reports', 'organizations', 'avg-delay-appointment', 'avg-delay-reports']
     },
 
     // Administrador:
@@ -59,7 +59,7 @@ const rolePermissions = {
         mail                    : ['send'],
         exporter                : [],
         wezen                   : ['studyToken'],
-        stats                   : ['appointment_requests', 'appointments', 'performing', 'reports', 'organizations', 'avg-delay-appointment']
+        stats                   : ['appointment_requests', 'appointments', 'performing', 'reports', 'organizations', 'avg-delay-appointment', 'avg-delay-reports']
     },
 
     // Supervisor:
@@ -468,6 +468,11 @@ const concessionPermissions = {
     // 26 : Estadísticas sobre demora de citas coordinadas:
     26: {
         stats                   : ['avg-delay-appointment']
+    },
+
+    // 27 : Estadísticas sobre demora de entrega de informes:
+    27: {
+        stats                   : ['avg-delay-reports']
     }
 }
 //--------------------------------------------------------------------------------------------------------------------//
