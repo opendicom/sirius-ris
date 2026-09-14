387fd663d6f2200015a54ef289a739edf9caa8a7
Mia <miaworkit@gmail.com>
2026-09-14 14:00:31 -0300

    Restructure of the medical locker
    "frontend/src/app/modules/performing/components/medical-locker/reporting-assignment" and "frontend/src/app/modules/performing/components/medical-locker/locker".
    -Add of the doctor assignment tab

---
+++ frontend/src/app/modules/performing/components/medical-locker/reporting-assignment/reporting-assignment.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reporting-assignment',
+  templateUrl: './reporting-assignment.component.html',
+  styleUrls: ['./reporting-assignment.component.css']
+})
+export class ReportingAssignmentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
